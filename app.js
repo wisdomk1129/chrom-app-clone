96b147f38d056d1b92e5f597974fd3e41bebef82
wisdomk1129 <wisdomk1129@naver.com>
2023-01-01 23:44:54 +0900

    loginform modify

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+const loginFrom = document.querySelector(".login-form");
 const loginInput = document.querySelector("input");
-const loginButton = document.querySelector("button");
+const greeting = document.querySelector(".greeting");
+const HIDDEN_CLASSNAME = "hidden"
 
-function onLoginButtonClick() {
-    if (loginInput.value === "") {
-        alert("Plese write your name :(");
+function onloginFormSubmit(event) {
+    event.preventDefault();
+    if (localStorage.length === 0) {
+        localStorage.setItem("userName", loginInput.value);
+        loginFrom.classList.add("hidden");
+        const userName = localStorage.getItem("userName");
+        greeting.innerText = `Hello! ${userName}`
+        greeting.classList.remove("hidden");
     }
     else {
-        console.log("hello " + loginInput.value);
-    }
-};
 
-loginButton.addEventListener("click", onLoginButtonClick);
+    }
+}
+
+loginFrom.addEventListener("submit", onloginFormSubmit);
