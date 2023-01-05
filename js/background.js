8dd69c925d2c36de655bd540f3f9a393ada562e1
wisdomk1129 <wisdomk1129@naver.com>
2023-01-05 22:25:03 +0900

    background img

---
+++ js/background.js
@@ -0,0 +1,8 @@
+const imgsArray = ["ocean (1).jpg", "ocean (2).jpg", "ocean (3).jpg", "ocean (4).jpg"];
+const selectedImg = imgsArray[Math.floor(Math.random() * imgsArray.length)];
+const bgImg = document.createElement("img");
+
+bgImg.src = `img/${selectedImg}`;
+bgImg.classList.add("bg-img");
+
+document.body.appendChild(bgImg);
