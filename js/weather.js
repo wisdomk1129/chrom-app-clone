f49164d0f5513e0e6f53758d51274ee084b75866
wisdomk1129 <wisdomk1129@naver.com>
2023-01-08 02:32:13 +0900

    weather

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const temp = document.querySelector(".weather span:first-child");
+const weather = document.querySelector(".weather span:nth-child(2)");
+const city = document.querySelector(".weather span:last-child");
+
+function onGeoSuccess(userLocation) {
+    const lat = userLocation.coords.latitude;
+    const lon = userLocation.coords.longitude;
+    const apiCall = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(apiCall)
+        .then(response => response.json())
+        .then(data => {
+            temp.innerText = data.main.temp;
+            weather.innerText = data.weather[0].main;
+            city.innerText = data.name;
+        })
+}
+
+function onGeoFail() {
+    alert("Can't find location. Weather information is not provided.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoFail);
